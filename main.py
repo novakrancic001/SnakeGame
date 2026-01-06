d524874bdc74696b051d56c58f91994dd61783ce
Klaja <novakrancic001@gmail.com>
2026-01-07 00:32:08 +0100

    First steps - base settings + Snake and Food class

---
+++ main.py
@@ -0,0 +1,84 @@
+from tkinter import *
+from tkextrafont import Font
+from pathlib import Path
+import random
+
+
+# GAME SETTINGS
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = '#ebe134'
+FOOD_COLOR = '#eb4034'
+BACKGROUND_COLOR = '#000000'
+
+# CLASSES
+class Snake:
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0,0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR)
+            self.squares.append(square)
+
+class Food:
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH//SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT//SPACE_SIZE)-1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR)
+
+# FUNCTIONS
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'right'
+
+font_path = Path(__file__).parent / 'RetroGaming.ttf'
+custom_font = Font(file = str(font_path), family = "Retro Gaming", weight = 'bold', size = 30)
+
+label  = Label(window, text = f"Score: {score}", font = custom_font)
+label.pack()
+
+canvas = Canvas(window, bg = BACKGROUND_COLOR, width = GAME_WIDTH, height = GAME_HEIGHT)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+window.mainloop()
